be0dd0b25542606404a05cd9ba8cb01b9f7e3755
hadeeljamal <hadeeljamal656@gmail.com>
2021-10-17 17:53:13 +0300

    landing page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function img(params) {
+    document.querySelector('.starbucks').src = params;
+}
+
+function changecolor(color) {
+   const circle = document.querySelector('.circle');
+   circle.style.background = color;
+}
